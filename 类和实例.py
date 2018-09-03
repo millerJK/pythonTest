ccf4533d16affee2653e03acef61d621cd1a9303
王宁 <wangning@cnepay.com>
2018-09-03 10:28:04 +0800

    修改私有属性访问限制文件

--- 类和实例.py
+++ 类和实例.py
@@ -1,23 +1,23 @@
 class Student(object):
 
     def __init__(self, name, score):
-        self.name = name;
-        self.score = score;
+        self.name = name
+        self.score = score
 
     def toString(self):
         print(self.name, self.score)
 
     def getScore(self):
-        if(self.score >= 90):
-            return 'A';
-        elif (self.score >=80):
-            return 'B';
+        if (self.score >= 90):
+            return 'A'
+        elif (self.score >= 80):
+            return 'B'
         else:
-            return 'C';
+            return 'C'
 
 
-bart = Student('jack', 23);
-bart.toString();
+bart = Student('jack', 23)
+bart.toString()
 print(bart.getScore())
-bart.score = 24; #可以通过调用类的属性直接修改属性值
-bart.toString();
+bart.score = 24  # 可以通过调用类的属性直接修改属性值
+bart.toString()
--- 类访问限制.py
+++ 类访问限制.py
@@ -1,9 +1,11 @@
 class Student(object):
 
-    # self 参数使用 __ 表示私有参，外部是不可以调用属性值进行修改
-    def __init__(self, name, score):
+    # self 参数使用 __ 表示私有属性，外部是不可以直接调用属性的，只能通过get set 的方法修改属性
+    #  __age__ 不是私有属性，对象还可以直接调用属性进行赋值  __name 才是私有属性
+    def __init__(self, name, score, age):
         self.__name = name;
         self.__score = score;
+        self.__age__ = age
 
     def get_name(self):
         return self.__name;
@@ -21,7 +23,7 @@ class Student(object):
             raise ValueError("数值不正确");
 
     def toString(self):
-        print(self.__name, self.__score)
+        print(self.__name, self.__score, self.__age__)
 
     def getScore(self):
         if (self.__score >= 90):
@@ -32,9 +34,11 @@ class Student(object):
             return 'C';
 
 
-student = Student("jack", 89);
+student = Student("jack", 89, 23);
 student.toString()
 print(student.get_name())
 student.set_name("是")
-student.set_score(1000)
+student.set_score(100)
+student.toString()
+student.__age__ = 12;
 student.toString()
